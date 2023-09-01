03cf851250f3ba8cc15b94ede4a67e863b004fdd
Demorvus <demovus@gmail.com>
2023-09-01 16:08:02 -0600

    first commit

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,18 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Class!");
+		
+		System.out.println();
+		int num1 = 66;
+		int num2 = 12;
+		int feet = num1 / num2;
+		int inches = num1 % num2;
+		System.out.println("The sum of the two numbers" + feet + "ft and " + inches + "in");
+
+		
+	}
+
+}
