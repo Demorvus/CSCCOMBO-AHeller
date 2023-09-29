8ac945bc0a8b9e159f000017e433f3a547be0f08
Demorvus <demovus@gmail.com>
2023-09-29 17:05:01 -0600

    Exam1

---
+++ Exam1/src/Exam1.java
@@ -0,0 +1,76 @@
+public class Exam1 {
+	import java.util.Scanner;
+	public static void main(String[] args) {
+//			System.out.println("this is my first programming test");
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please enter a number to choose a beverage:");
+//        System.out.println("(1) Water");
+//        System.out.println("(2) Coke");
+//        System.out.println("(3) Coffee");
+//        int choice = scanner.nextInt();
+//        String beverage;
+//        
+//        switch (choice) {
+//            case 1:
+//                beverage = "Water";
+//                break;
+//            case 2:
+//                beverage = "Coke";
+//                break;
+//            case 3:
+//                beverage = "Coffee";
+//                break;
+//            default:
+//                beverage = "Invalid choice";
+//                break;
+//        }
+//        
+//        System.out.println("You chose: " + beverage);
+//    }
+//    public static boolean makes10(int num1, int num2) {
+//        return (num1 + num2) == 10;
+//    }
+//
+//        int number1 = 4;
+//        int number2 = 6;
+//
+//        if (makes10(number1, number2)) {
+//            System.out.println("The numbers " + number1 + " and " + number2 + " add up to 10.");
+//        } else {
+//            System.out.println("The numbers " + number1 + " and " + number2 + " do not add up to 10.");
+//        }
+//    }
+//}
+//function PrintNumber() {
+//	  for (let i = 5; i <= 19; i++) {
+//	    console.log(i);
+//	  }
+//	}
+//
+//	PrintNumber();
+//{
+//let numbers = [];
+//{
+//let number = parseInt(prompt("Enter a number (or 0 to stop):"));
+//{
+//while (number != 0) {
+//numbers.push(number);
+//
+//number = parseInt(prompt("Enter a number (or 0 to stop):"));
+//
+//console.log("Numbers entered:");
+//for (let i = 0; i < numbers.length; i++) {
+//console.log(numbers[i]);
+//}
+//
+//let sum = 0;
+//for (let i = 0; i < numbers.length; i++) {
+//sum += numbers[i];
+//}
+//console.log("Sum of all the numbers:", sum);
+//}
+}
+}
+
+
+
---
+++ Exam1/src/Exam1Final.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class Exam1Final {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my first programming test");
+//		**Problem 2
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please enter a number to choose a beverage:");
+		System.out.println("(1) Water");
+		System.out.println("(2) Coke");
+		System.out.println("(3) Coffee");
+		int choice = scanner.nextInt();
+		String beverage;
+
+		switch (choice) {
+		case 1:
+			beverage = "Water";
+			break;
+		case 2:
+			beverage = "Coke";
+			break;
+		case 3:
+			beverage = "Coffee";
+			break;
+		default:
+			beverage = "Invalid choice";
+			break;
+		}
+
+		System.out.println("You chose: " + beverage);
+//		**Problem 3
+        int num1 = 5;
+        int num2 = 5;
+        
+        if (makes10(num1, num2)) {
+            System.out.println("The numbers " + num1 + " and " + num2 + " add up to 10.");
+        } else {
+            System.out.println("The numbers " + num1 + " and " + num2 + " do not add up to 10.");
+        }
+    }
+    
+    public static boolean makes10(int a, int b) {
+        return (a + b) == 10;
+    
+//	** Problem 4
+    for (int i = 5; i <= 19; i++) {
+        System.out.println(i);
+    }
+//    ** Problem 5
+//    boolean accumulator() {
+//        numbers = [];
+//        sum = 0;
+//        number = parseInt(prompt("Enter a number: "));
+//
+//        while (number !== 0) {
+//            numbers.push(number);
+//            sum += number;
+//            number = parseInt(prompt("Enter a number: "));
+//        }
+//
+//        console.log("All numbers entered: " + numbers.join(", "));
+//        console.log("Sum of all numbers: " + sum);
+//    }
+//
+//    accumulator();
+//    
+}
+}
